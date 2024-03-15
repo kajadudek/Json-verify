02f4d0c3e6f9885d84405cf7a64ca0c8bf23f3fe
Kaja Dudek <410170@student.agh.edu.pl>
2024-03-15 12:42:39 +0100

    Initial commit

---
+++ src/main/java/com/json_parser/parser/JsonParserApplication.java
@@ -0,0 +1,13 @@
+package com.json_parser.parser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonParserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsonParserApplication.class, args);
+    }
+
+}
